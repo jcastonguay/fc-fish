ca5c0b14a7f2cf4669856f74a70159ec0929c170
Jason Castonguay <jcastonguay>
2024-11-04 14:57:52 -0500

    changes to be more posix and how I like to use fc

--- functions/fc.fish
+++ functions/fc.fish
@@ -1,13 +1,23 @@
 function fc --description 'Edit the command history list in EDITOR'
-    set -l options l/list r/reverse
+    #TODO: prevent fc from being a part of the history
+    #TODO: add command substitution with -s optional foo=bar
+
+    set -l options l/list r/reverse n/nonumbers s/noedit e/editor=
     argparse -n fc --max-args=2 -i $options -- $argv
     or return
 
-    if set -q _flag_reverse
+    if set -q _flag_nonumbers
         if not set -q _flag_list
-            echo fc: -r should work with -l
+            echo fc: option requires -l
             return 1
         end
+    end
+
+    # swap
+    if set -q _flag_reverse
+        set -l temp $start_index
+        set start_index $end_index
+        set end_index $temp
     end
 
     if set -q _flag_list
@@ -15,45 +25,54 @@ function fc --description 'Edit the command history list in EDITOR'
         set -l end_index 16
 
         if test $(count $argv) -eq 1
-            set end_index (math -$argv[1])
+            set end_index (math $argv[1])
         else if test $(count $argv) -eq 2
-            set start_index (math -$argv[1])
-            set end_index (math -$argv[2])
+            set start_index (math $argv[1])
+            set end_index (math $argv[2])
         end
 
-        # swap
-        if set -q _flag_reverse
-            set -l temp $start_index
-            set start_index $end_index
-            set end_index $temp
-        end
-
-        for i in $history[$start_index..$end_index]
-            echo $i
+        if set -q _flag_nonumbers
+            for i in (seq $start_index $end_index)
+                echo "$history[$i]"
+            end
+        else
+            for i in (seq $start_index $end_index)
+                echo "$i  $history[$i]"
+            end
         end
         return 0
+    end
+
+    if set -q _flag_editor
+        set -fx VISUAL $_flag_editor
     end
 
     if test $(count $argv) -eq 0
         commandline -t $history[1]
-        edit_command_buffer
+        if not set -q _flag_noedit
+            edit_command_buffer
+        end
         return 0
     end
 
     if test $(count $argv) -eq 1
-        commandline -t $history[(math -$argv[1])]
-        edit_command_buffer
+        commandline -t $history[(math $argv[1])]
+        if not set -q _flag_noedit
+            edit_command_buffer
+        end
         return 0
     end
 
     if test $argv[1] -eq 0
-      set argv[1] -1
+        set argv[1] 1
     end
 
     if test $argv[2] -eq 0
-      set argv[2] -1
+        set argv[2] 1
     end
 
-    commandline -r -- $history[(math -$argv[1])..(math -$argv[2])]
-    edit_command_buffer
+    commandline -r -- $history[(math $argv[1])..(math $argv[2])]
+    if not set -q _flag_noedit
+        edit_command_buffer
+    end
 end
